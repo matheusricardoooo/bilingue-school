aa869acfc1daf854c1efa1ae983d8d7b08e20acb
matheusricardooooo <matheusricardodev@gmail.com>
2025-09-02 21:04:13 -0300

    adiconando as setas do btn da section de idiomas e importando a pasta JS no html

---
+++ script.js
@@ -0,0 +1,5 @@
+    const slides = document.querySelectorAll('.carrossel');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+    let slideAtual = 0;
+    const tempoAuto = 5000; // 5 segundos
