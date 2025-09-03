d916e81cc81d8fc4f965786ff354ec7420c6d826
matheusricardooooo <matheusricardodev@gmail.com>
2025-09-03 20:56:45 -0300

    código js adicionado

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-    const slides = document.querySelectorAll('.carrossel');
-    const prevBtn = document.getElementById('prevBtn');
-    const nextBtn = document.getElementById('nextBtn');
-    let slideAtual = 0;
-    const tempoAuto = 5000; // 5 segundos
+const carrosselAtivo = document.querySelector('.carrossel-ativo');
+const carrosselSecundario = document.querySelector('.carrossel');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let isFirstCarrosselActive = true
+
+function alternarCarrossel() {
+    if (isFirstCarrosselActive) {
+        carrosselAtivo.style.display = 'none';
+        carrosselSecundario.style.display = 'flex';
+    }
+
+    else {
+        carrosselAtivo.style.display = 'flex';
+        carrosselSegundario.style.display = 'none';
+    }
+
+    isFirstCarrosselActive = !isFirstCarrosselActive;
+}
+
+nextBtn.addEventListener('click', alternarCarrossel);
+prevBtn.addEventListener('click', alternarCarrossel);
+
+setInterval(alternarCarrossel, 5000);
